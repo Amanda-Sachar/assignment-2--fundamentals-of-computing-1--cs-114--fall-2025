c1392474c29ae053805f86defd5965cde4e26594
amanda-sachar <mandysachar@gmail.com>
2025-09-24 21:47:30 -0400

    tested things out (might be too much to explain in title so explained in description)
    added scanner
    added a counter for each vowel
    got input of string from user
    used while loop so as long as the length of string is not zero or less it will work
    each if/else if checks if the first letter is a vowel, if yes corresponding counter goes up
    after checking if the 1st value is a vowel, the first character is removed and the proccess repeats
    prints out counter of vowels (need to find how to make it not show after each round of checking)
    prints out how many non vowel/uppercased vowels were found by subtracting all the vowel counters from the starting length

--- PrintVowelsAndNonVowels.java
+++ PrintVowelsAndNonVowels.java
@@ -1,4 +1,61 @@
+import java.util.Scanner;
 public class PrintVowelsAndNonVowels {
   public static void main(String[] args) {
+    System.out.println("Please type a string:");
+    Scanner scan = new Scanner(System.in);
+    String stringGiven= scan.nextLine();
+    System.out.println("string entered is:"+stringGiven);
+    int aCounter=0;
+    int eCounter=0;
+    int iCounter=0;
+    int oCounter=0;
+    int uCounter=0;
+    // int indexLocation = 0;
+    // boolean isACheckDone=false;
+    // int timesACountRan=0;
+    // int timesECountRan=0;
+    // int timesICountRan=0;
+    // int timesOCountRan=0;
+    // int timesUCountRan=0;
+    String stringLeftOver=stringGiven;
+    while(stringLeftOver.length()>0){
+      if (stringLeftOver.charAt(0)=='a'){
+        ++aCounter;}
+        else if (stringLeftOver.charAt(0)=='e'){
+          ++eCounter;
+        }
+        else if (stringLeftOver.charAt(0)=='i'){
+          ++iCounter;
+        }
+        else if (stringLeftOver.charAt(0)=='o'){
+          ++oCounter;
+        }
+        else if (stringLeftOver.charAt(0)=='u'){
+          ++uCounter;
+        }
+  stringLeftOver= stringLeftOver.substring(1);
+  System.out.println("a="+aCounter+"     e="+eCounter     +"     i="+iCounter+
+      "     o="+oCounter+"     u="+uCounter);
+  System.out.println("the amount of non-vowels and/or not lowercase characters is:"+ (stringGiven.length()-
+      (aCounter+eCounter+iCounter+oCounter+uCounter)) );
+      //System.out.println("  the string left is: "+stringLeftOver);
+
+      }
+    //int lengthLeft=stringGiven.length();
+    //System.out.println("the starting lenght is: "+stringGiven.length());
+    // while(timesACountRan<stringGiven.length()){
+    //     System.out.println("first character="+
+    //     stringLeftOver.charAt(0));
+    //   if (stringLeftOver.charAt(0)=='a');
+    //     aCounter=aCounter+1;
+    //     indexLocation=indexLocation+1;
+    //     timesACountRan++;
+    //     stringLeftOver=stringLeftOver.substring(1);
+    //     System.out.println("the a count is: "+aCounter);
+    //     System.out.println("the string after is: "+stringLeftOver);
+    //     //System.out.println("the amount of times a counter has ran is "+
+    //     //timesACountRan);
+    //}
+    scan.close();
   }
 }
