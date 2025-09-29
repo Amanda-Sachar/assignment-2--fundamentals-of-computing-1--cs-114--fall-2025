a8cf0e33e7afa09025d879d3c51572bbcebe8b3c
amanda-sachar <mandysachar@gmail.com>
2025-09-29 17:54:28 -0400

    Scanner, ask for times to repeat, for loop, print
    Added scanner
    Ask user for times to repeat
    Have a for loop that runs until the times the for loop is called is not less than the times requested to repeat
    Print a string that has 100-the amount of times called, then at end add 1 to times called

--- OneHundredBottlesOfBeer.java
+++ OneHundredBottlesOfBeer.java
@@ -1,4 +1,16 @@
+import java.util.Scanner;
+
 public class OneHundredBottlesOfBeer {
   public static void main(String[] args) {
+    Scanner scan = new Scanner(System.in);
+    System.out.println("Please enter number of repeats:");
+    int repeats= scan.nextInt();
+    for (int timesCalled=0; timesCalled < repeats;){
+      System.out.print(Integer.toString(100-timesCalled) + " bottles of beer on the wall\r\n"+
+      Integer.toString(100-timesCalled) + " bottles of beer\r\n"+
+      "If one of those bottles should happen to fall\r\n"+
+      Integer.toString(100-(++timesCalled))+" bottles of beer on the wall\r\n\n");
+    };
+    scan.close();
   }
 }
