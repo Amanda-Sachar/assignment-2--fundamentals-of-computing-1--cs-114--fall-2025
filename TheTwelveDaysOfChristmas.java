d752c66f1f693a21e66564bcf1b86019d1a3eb55
amanda-sachar <mandysachar@gmail.com>
2025-09-29 19:00:35 -0400

    Removed Array, preventing invalid numbers
    Removed Array
    Replaced Array with String
    Put new line at end of each case
    Prevented poem lines from being written if invalid number is entered

--- TheTwelveDaysOfChristmas.java
+++ TheTwelveDaysOfChristmas.java
@@ -5,34 +5,36 @@ public class TheTwelveDaysOfChristmas {
     Scanner scan = new Scanner(System.in);
     System.out.println("Please enter the day of Christmas:");
     int day= scan.nextInt();
-    java.util.ArrayList<String> Gifts = new java.util.ArrayList<String>();
-
+    //java.util.ArrayList<String> Gifts = new java.util.ArrayList<String>();
+    String Poem="\n";
     switch (day) {
-      case 12: Gifts.add("Twelve drummers drumming, ");
-      case 11: Gifts.add("Eleven pipers piping, ");
-      case 10: Gifts.add("Ten lords a-leaping, ");
-      case 9: Gifts.add("Nine ladies dancing, ");
-      case 8: Gifts.add("Eight maids a-milking, ");
-      case 7: Gifts.add("Seven swans a-swimming, ");
-      case 6: Gifts.add("Six geese a-laying, ");
-      case 5: Gifts.add("Five golden rings, ");
-      case 4: Gifts.add("Four calling birds, ");
-      case 3: Gifts.add("Three French hens, ");
-      case 2: Gifts.add("Two turtle doves, and");
-      case 1: Gifts.add("A partridge in a pear tree");
+      case 12: Poem +="Twelve drummers drumming, \n";
+      case 11: Poem +="Eleven pipers piping, \n";
+      case 10: Poem +="Ten lords a-leaping, \n";
+      case 9: Poem +="Nine ladies dancing, \n";
+      case 8: Poem +="Eight maids a-milking, \n";
+      case 7: Poem +="Seven swans a-swimming, \n";
+      case 6: Poem +="Six geese a-laying, \n";
+      case 5: Poem +="Five golden rings, \n";
+      case 4: Poem +="Four calling birds, \n";
+      case 3: Poem +="Three French hens, \n";
+      case 2: Poem +="Two turtle doves, and\n";
+      case 1: Poem +="A partridge in a pear tree";
 
 break;
+default:
+      System.out.println("Invalid Day Entered");
     };
     if (day==1){
       System.out.println("On the "+day+"st day of Christmas my true love gave to me "+
-    Gifts);}
+    Poem);}
     else if (day==2){
       System.out.println("On the "+day+"nd day of Christmas my true love gave to me "+
-    Gifts);
+    Poem);
     }else if (day==3){
-      System.out.println("On the "+day+"rd day of Christmas my true love gave to me "+Gifts);
-    }else{
-      System.out.println("On the "+day+"th day of Christmas my true love gave to me "+Gifts);
+      System.out.println("On the "+day+"rd day of Christmas my true love gave to me "+Poem);
+    }else if (day>0&&day<13){
+      System.out.println("On the "+day+"th day of Christmas my true love gave to me "+Poem);
     }
     scan.close();
   }
