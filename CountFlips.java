c2b0647de86c837b0abfaa803904ca271b9f4ccc
amanda-sachar <mandysachar@gmail.com>
2025-09-30 16:39:34 -0400

    Made counters, For loops,  Print statements
    Made counters for times ran and heads flipped
    Made variable for result of flipped coin
    Made for loop for when the times ran is less than 100
    Made Print statements for times heads and tails were flipped with total times coin was flipped

--- CountFlips.java
+++ CountFlips.java
@@ -1,4 +1,22 @@
 public class CountFlips {
   public static void main(String[] args) {
+//have computer choose 0 or 1, 0=headCount+1, 1=tailCount+1
+//or just skip tail count and do 100-headCount
+    int timesRan;
+    int headCounter=0;
+    int flipResult;
+    for (timesRan=0; timesRan<100;)
+    {
+      flipResult=(int) (Math.random() * 2);
+      if (flipResult==0){
+        ++headCounter;
+      }
+      ++timesRan;
+    }
+    if (timesRan>=100){
+      System.out.println("Heads was flipped "+headCounter+" times");
+      System.out.println("Tails was flipped "+(100-headCounter)+" times");
+      System.out.println("the coin was flipped "+timesRan+" times overall");
+    }
   }
 }
