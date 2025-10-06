9f16ef08b802232860322d52c6dc0069744dd5e3
amanda-sachar <mandysachar@gmail.com>
2025-10-06 16:20:57 -0400

    added coin.java and consistencies
    Figured out how to use coin.java in CountFlips.java
    Removed previous way coins were flipped
    Made spaces around all binary operators consistent
    Removed unneeded comments

--- CountFlips.java
+++ CountFlips.java
@@ -1,30 +1,21 @@
 public class CountFlips {
   public static void main(String[] args) {
-//have computer choose 0 or 1, 0=headCount+1, 1=tailCount+1
-//or just skip tail count and do 100-headCount
-    int timesRan;
-    int headCounter=0;
-    int flipResult;
 
-    for (timesRan=0; timesRan<100;timesRan++){
-      flipResult=(int) (Math.random() * 2);
-      if (flipResult==0){
+    int timesRan;
+    int headCounter = 0;
+    Coin coin = new Coin();
+
+    for (timesRan = 0;timesRan < 100;timesRan++) {
+      coin.flip();
+      if (coin.isHeads()) {
         ++headCounter;
       }
-
     }
-/*I know I should have used Coin.java for this but I was having
- * difficulty figuring out how to use it here. So I ended up
- * creating my own way to do it. I will redo all of this if
- * doing so is not allowed
- * EDIT: I have found how to do it through the examples Repo,
- * but I feel like just changing all my work to be what is done there
- * feels like cheating or that the work isn't mine if all I do
- * is make some minor adjustments*/
-    if (timesRan>=100){
-      System.out.println("Heads was flipped "+headCounter+" times");
-      System.out.println("Tails was flipped "+(100-headCounter)+" times");
-      System.out.println("the coin was flipped "+timesRan+" times overall");
+
+    if (timesRan >= 100) {
+      System.out.println("Heads was flipped " + headCounter + " times");
+      System.out.println("Tails was flipped " + (100-headCounter) + " times");
+      System.out.println("the coin was flipped " + timesRan + " times overall");
     }
   }
 }
